e3c81344390d3e70640bf02b1c471ccdda5e665e
nicoclassique <nicoclassique@hotmail.fr>
2023-07-11 13:15:07 +0000

    Initialisation

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ParticipantController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [ParticipantController::class, 'index'])->name('participants.index');
+Route::get('/participants/create', [ParticipantController::class, 'create'])->name('participants.create');
+Route::post('/participants', [ParticipantController::class, 'store'])->name('participants.store');
+
